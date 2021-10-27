494dd20c272d6dd667a1a5d8a7bdc431c2b6a7e2
weblinuxgame <15975798646@163.com>
2021-10-27 15:04:56 +0800

    update swagger

--- transport/api/manager.go
+++ transport/api/manager.go
@@ -10,25 +10,53 @@ type QueueManagerApi interface {
 	// CreateConsumer godoc
 	// @Summary 创建队列消费器
 	// @Tags QueueMgrServ
-	// @Description register live app info
+	// @Description create queue consumer
 	// @Accept  x-www-form-urlencoded
 	// @Produce  json
 	// @Param Authorization header string true "access jwt token"
-	// @Param app_name formData string true "app name"
-	// @Param app_logo_url formData string false "app logo url"
-	// @Param callback_url formData string false "callback url"
-	// @Param sync_url formData string false "sync lives api"
-	// @Param proxy_url formData string false "proxy url addr"
-	// @Param sync_type formData int false "sync type ( 1:post_sync,2:interval_sync )" Enums(1, 2) default(1)
-	// @Param interval_duration formData int false "interval sync duration (unit: second)" Enums(0,10,15,30,60,180)
-	// @Param interval_fail_times formData int false "interval fail retry times" Enums(0,3,5,10) default(0)
-	// @Param extras formData string false "extras data " default({"pro_im_url":"","test_im_url":"","dev_im_url":""})
+	// @Param consumer formData string true "consumer/消费器名"
+	// @Param type formData string true "type/消费器类型" Enums("FastCGI","Native","Shell","Api","Grpc","Proxy","Plugins")
+	// @Param properties formData string false "properties/消费器相关参数(json)"
 	// @Success 200 {object} entity.JsonResponse
 	// @Failure 400,404 {object} entity.JsonResponse
 	// @Failure 500 {object} entity.JsonResponse
 	// @Failure default {object} entity.JsonResponse
 	// @Router /consumer/create [post]
 	CreateConsumer(ctx *fiber.Ctx) error
+
+	// CreateQueue godoc
+	// @Summary 创建可消费队列信息
+	// @Tags QueueMgrServ
+	// @Description add queue  info
+	// @Accept  x-www-form-urlencoded
+	// @Produce  json
+	// @Param Authorization header string true "access jwt token"
+	// @Param queue formData string true "queue/队列名"
+	// @Param driver formData string true "queue/队列链接驱动器类型" Enums("AMQP","MQTT","HTTP","WS","PLUGINS")
+	// @Param properties formData string false "properties/可消费队列相关参数(json)"
+	// @Success 200 {object} entity.JsonResponse
+	// @Failure 400,404 {object} entity.JsonResponse
+	// @Failure 500 {object} entity.JsonResponse
+	// @Failure default {object} entity.JsonResponse
+	// @Router /queue/create [post]
+	CreateQueue(ctx *fiber.Ctx) error
+
+	// Bind godoc
+	// @Summary 给队列绑定消费协程
+	// @Tags QueueMgrServ
+	// @Description bind queue consumer
+	// @Accept  x-www-form-urlencoded
+	// @Produce  json
+	// @Param Authorization header string true "access jwt token"
+	// @Param consumer formData string false "consumer/消费器名"
+	// @Param queue formData string false "queue/队列名"
+	// @Param properties formData string false "properties/绑定消费器相关参数(json)"
+	// @Success 200 {object} entity.JsonResponse
+	// @Failure 400,404 {object} entity.JsonResponse
+	// @Failure 500 {object} entity.JsonResponse
+	// @Failure default {object} entity.JsonResponse
+	// @Router /bind [post]
+	Bind(ctx *fiber.Ctx) error
 
 	// State godoc
 	// @Summary 查询队列消费器状态
@@ -37,16 +65,9 @@ type QueueManagerApi interface {
 	// @Accept  x-www-form-urlencoded
 	// @Produce  json
 	// @Param Authorization header string true "access jwt token"
-	// @Param app_id formData string true "app id"
-	// @Param app_name formData string false "app name"
-	// @Param app_logo_url formData string false "app logo url"
-	// @Param callback_url formData string false "callback url"
-	// @Param proxy_url formData string false "proxy url addr"
-	// @Param sync_url formData string false "sync lives api"
-	// @Param sync_type formData int false "sync type ( 1:post_sync,2:interval_sync )" Enums(1, 2)
-	// @Param interval_duration formData int false "interval sync duration (unit: second)" Enums(10,15,30,60,180)
-	// @Param interval_fail_times formData int false "interval fail retry times" Enums(0,3,5,10)
-	// @Param extras formData string false "extras data" default({"pro_im_url":"","test_im_url":"","dev_im_url":""})
+	// @Param queue query string true "queue/消费队列名"
+	// @Param tag query string false "tag/消费进程标签"
+	// @Param state query int false "state/消费进程状态" Enums(1,2,3)
 	// @Success 200 {object} entity.JsonResponse
 	// @Failure 400,404 {object} entity.JsonResponse
 	// @Failure 500 {object} entity.JsonResponse
@@ -54,24 +75,17 @@ type QueueManagerApi interface {
 	// @Router /state [get]
 	State(ctx *fiber.Ctx) error
 
-
 	// Control godoc
 	// @Summary 控制队列消费器状态
 	// @Tags QueueMgrServ
-	// @Description change queue consumer state
+	// @Description change queue consumer state or processor number
 	// @Accept  x-www-form-urlencoded
 	// @Produce  json
 	// @Param Authorization header string true "access jwt token"
-	// @Param app_id formData string true "app id"
-	// @Param app_name formData string false "app name"
-	// @Param app_logo_url formData string false "app logo url"
-	// @Param callback_url formData string false "callback url"
-	// @Param proxy_url formData string false "proxy url addr"
-	// @Param sync_url formData string false "sync lives api"
-	// @Param sync_type formData int false "sync type ( 1:post_sync,2:interval_sync )" Enums(1, 2)
-	// @Param interval_duration formData int false "interval sync duration (unit: second)" Enums(10,15,30,60,180)
-	// @Param interval_fail_times formData int false "interval fail retry times" Enums(0,3,5,10)
-	// @Param extras formData string false "extras data" default({"pro_im_url":"","test_im_url":"","dev_im_url":""})
+	// @Param queue formData string true "queue/消费队列名"
+	// @Param state formData int false "state/消费进程状态" Enums(1,2,3)
+	// @Param tag formData string false "tag/消费进程标签"
+	// @Param scale formData int false "scale/消费队列协程数扩缩容" default(0)
 	// @Success 200 {object} entity.JsonResponse
 	// @Failure 400,404 {object} entity.JsonResponse
 	// @Failure 500 {object} entity.JsonResponse
@@ -79,4 +93,22 @@ type QueueManagerApi interface {
 	// @Router /state/update [post]
 	Control(ctx *fiber.Ctx) error
 
+	// Lists godoc
+	// @Summary 罗列消费队列信息
+	// @Tags QueueMgrServ
+	// @Description query queue consumer lists
+	// @Accept  x-www-form-urlencoded
+	// @Produce  json
+	// @Param Authorization header string true "access jwt token"
+	// @Param page formData int false "page/页码" default(1)
+	// @Param count query int false "count/分页量" default(10)
+	// @Param state query int false "state/消费进程状态" Enums(0,1,2,3)
+	// @Param sort query string false "sort/排序参数" default("created_at:desc")
+	// @Param queue query string false "name/限定队列名(模糊匹配eg: test*)"
+	// @Success 200 {object} entity.JsonResponse
+	// @Failure 400,404 {object} entity.JsonResponse
+	// @Failure 500 {object} entity.JsonResponse
+	// @Failure default {object} entity.JsonResponse
+	// @Router /consumers [get]
+	Lists(ctx *fiber.Ctx) error
 }
--- transport/api/router.go
+++ transport/api/router.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 type RouterApi interface {
 	// ListRouter godoc
 	// @Summary 罗列服务接口列表
-	// @Tags AppCdnServ
+	// @Tags QueueMgrServ
 	// @Description List Service Api Routers
 	// @Produce  json
 	// @Success 200 {object} entity.KvMap
@@ -14,4 +14,13 @@ type RouterApi interface {
 	// @Failure default {object} entity.KvMap
 	// @Router /routers [get]
 	ListRouter(ctx *fiber.Ctx) error
+
+	// Metrics godoc
+	// @Summary export prometheus metrics
+	// @Tags QueueMgrServ
+	// @Description prometheus Metrics
+	// @Produce  plain
+	// @Router /metrics [get]
+	Metrics(ctx *fiber.Ctx) error
+
 }
